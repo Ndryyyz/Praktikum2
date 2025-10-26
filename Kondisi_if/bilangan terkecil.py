a7c914a7d3763828f51a08a7a1b531283e12e339
Ndryyyz <andriwibowo2006@gmail.com>
2025-10-27 03:06:07 +0700

    Update bilangan terkecil.py

--- Kondisi_if/bilangan terkecil.py
+++ Kondisi_if/bilangan terkecil.py
@@ -1,6 +1,6 @@
 # Program sederhana untuk menemukan bilangan terbesar dari 4 bilangan menggunakan if statements
 
-# Input 4 bilanganbil
+# Input 4 bilangan
 bilangan1 = int(input("Masukkan bilangan pertama: "))
 bilangan2 = int(input("Masukkan bilangan kedua: "))
 bilangan3 = int(input("Masukkan bilangan ketiga: "))
@@ -22,4 +22,5 @@ if bilangan2 > bilangan3:
 if bilangan1 > bilangan2:
     bilangan1, bilangan2 = bilangan2, bilangan1
 # Output bilangan terkecil ke terbesar
+
 print("urutan bilangan:", bilangan1, bilangan2, bilangan3, bilangan4)

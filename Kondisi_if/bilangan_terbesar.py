a7a4137f8aff8012a8faa8ff67e598fb7facaad8
Ndryyyz <andriwibowo2006@gmail.com>
2025-10-27 11:12:00 +0700

    Update bilangan_terbesar.py

--- Kondisi_if/bilangan_terbesar.py
+++ Kondisi_if/bilangan_terbesar.py
@@ -7,17 +7,16 @@ bilangan3 = int(input("Masukkan bilangan ketiga: "))
 bilangan4 = int(input("Masukkan bilangan keempat: "))
 
 # Gunakan bilangan1 sebagai pembanding
-# Bandingkan dengan bilangan2, bilangan3 dst..
+# Bandingkan dengan bilangan2
 if bilangan2 > bilangan1:
     bilangan1 = bilangan2
 
-elif bilangan3 > bilangan1:
+if bilangan3 > bilangan1:
     bilangan1 = bilangan3
     
-elif bilangan4 > bilangan1:
+if bilangan4 > bilangan1:
     bilangan1 = bilangan4
 
 
 # Output bilangan terbesar
 print(f"Bilangan terbesar adalah: {bilangan1}")
-
